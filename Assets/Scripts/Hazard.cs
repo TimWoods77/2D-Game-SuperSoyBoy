1f018bf0f241c7444e8fbdcf3480c7fc0a9a6f8d
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-11-07 21:33:22 -0500

    made a death particle prefab and activated the buzzsaw so when soyboy hits it the death sound hits and the particles fly

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+
+    public GameObject playerDeathPrefab;
+    public AudioClip deathClip;
+    public Sprite hitSprite;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        // 1  
+        if (coll.transform.tag == "Player")// check to ensure the colliding GameObject has the "Player" tag.
+        {
+
+            // 2   
+            var audioSource = GetComponent<AudioSource>();//Next, determine if an AudioClip has been assigned to the script and that a valid Audio Source component exists.
+            if (audioSource != null && deathClip != null) 
+            {
+                audioSource.PlayOneShot(deathClip);//If so, play the deathClip audio effect.
+            }
+
+            // 3  
+            Instantiate(playerDeathPrefab, coll.contacts[0].point,//Instantiate the playerDeathPrefab at the collision point and swap the sprite of the saw blade with the hitSprite version.
+                Quaternion.identity);
+            spriteRenderer.sprite = hitSprite;
+
+            // 4  
+            Destroy(coll.gameObject);//Lastly, destroy the colliding object (the player).
+        }
+    }
+}
---
+++ Assets/Scripts/RotationScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+    public float rotationsPerMinute = 640f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationsPerMinute * Time.deltaTime, Space.Self);//This will rotate the GameObject the script is attached to around the Z-axis based on the value of rotationsPerMinute.
+    }
+}
